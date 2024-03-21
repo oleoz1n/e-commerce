2691a2d85f4e590529aa73b9f254fedc8d2a0f47
oleoz1n <leopity0@gmail.com>
2024-03-20 21:51:45 -0300

    Add Footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Footer() {
+    return (
+        <footer className="w-full h-48 items-center flex justify-center">
+            <p>&copy; Desenvolvido por oLeoz1n</p>
+        </footer>
+    );
+}
