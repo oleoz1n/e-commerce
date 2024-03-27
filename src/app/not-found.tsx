d3dd97ea5f0e46d76ce60dd56cec73e774b98097
oleoz1n <leopity0@gmail.com>
2024-03-27 16:46:19 -0300

    Add not-found component

---
+++ src/app/not-found.tsx
@@ -0,0 +1,20 @@
+import Link from "next/link";
+export default function notFound() {
+    return (
+        <section className="flex h-[90vh] flex-1 flex-col items-center justify-center gap-2">
+            <h1 className="text-5xl">Página não encontrada</h1>
+            <div>
+                <p className="text-2xl">
+                    Voltar para{" "}
+                    <Link
+                        scroll={false}
+                        className="text-blue-600 underline transition-all duration-300 hover:text-blue-800"
+                        href="/"
+                    >
+                        Home
+                    </Link>
+                </p>
+            </div>
+        </section>
+    );
+}
