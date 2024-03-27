e4cef614144f692fd9548e5c9e496604d34faeb4
oleoz1n <leopity0@gmail.com>
2024-03-27 16:48:41 -0300

    Update text alignment in not-found component

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 export default function notFound() {
     return (
-        <section className="flex h-[90vh] flex-1 flex-col items-center justify-center gap-2">
+        <section className="flex h-[90vh] flex-1 flex-col items-center justify-center gap-2 text-center">
             <h1 className="text-5xl">Página não encontrada</h1>
             <div>
                 <p className="text-2xl">
