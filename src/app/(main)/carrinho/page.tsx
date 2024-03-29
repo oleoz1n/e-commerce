3f7c5813e5cbde8d2bc3a92d339c9edfa668f88d
oleoz1n <leopity0@gmail.com>
2024-03-29 18:13:47 -0300

    update

--- src/app/(main)/carrinho/page.tsx
+++ src/app/(main)/carrinho/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import ProdutosCarrinho from "@/components/Carrinho/ProdutosCarrinho";
 import React from "react";
 
@@ -6,22 +7,26 @@ export default function Carrinho() {
         <main className="flex min-h-screen w-full flex-row max-lg:flex-col">
             <div className="flex w-2/3 flex-col items-center gap-2 max-lg:w-full">
                 <h1>Carrinho</h1>
-                <ProdutosCarrinho
-                    imagem={{
-                        src: "https://www.girafa.com.br/visao/default/img/produtos/Telefonia/Celulares/iphone-12-pro-apple-128gb-dourado-tela-6-1-camera-tripla-12mp-ios-896300-1625227020-1-preview.webp",
-                        alt: "Imagem",
-                    }}
-                    preco={32.99}
-                    nome="Iphone 12 adjkiopasdiohjaodj aosdhjoashdohasodh oashjdoashdohjao asdhjasojdoajd"
-                />
-                <ProdutosCarrinho
-                    imagem={{
-                        src: "https://www.girafa.com.br/visao/default/img/produtos/Telefonia/Celulares/iphone-12-pro-apple-128gb-dourado-tela-6-1-camera-tripla-12mp-ios-896300-1625227020-1-preview.webp",
-                        alt: "Imagem",
-                    }}
-                    preco={32.99}
-                    nome="Iphone 12 adjkiopasdiohjaodj aosdhjoashdohasodh oashjdoashdohjao asdhjasojdoajd"
-                />
+                <div>
+                    <ProdutosCarrinho
+                        imagem={{
+                            src: "https://www.girafa.com.br/visao/default/img/produtos/Telefonia/Celulares/iphone-12-pro-apple-128gb-dourado-tela-6-1-camera-tripla-12mp-ios-896300-1625227020-1-preview.webp",
+                            alt: "Imagem",
+                        }}
+                        preco={32.99}
+                        nome="Iphone 12 adjkiopasdiohjaodj aosdhjoashdohasodh oashjdoashdohjao asdhjasojdoajd"
+                        qtd={2}
+                    />
+                    <ProdutosCarrinho
+                        imagem={{
+                            src: "https://www.girafa.com.br/visao/default/img/produtos/Telefonia/Celulares/iphone-12-pro-apple-128gb-dourado-tela-6-1-camera-tripla-12mp-ios-896300-1625227020-1-preview.webp",
+                            alt: "Imagem",
+                        }}
+                        preco={1032.99}
+                        nome="Iphone 12 adjkiopasdiohjaodj aosdhjoashdohasodh oashjdoashdohjao asdhjasojdoajd"
+                        qtd={2}
+                    />
+                </div>
             </div>
             <div className="w-1/3 flex-col items-center max-lg:w-full">
                 <h2>Resume</h2>
--- src/components/Carrinho/ProdutosCarrinho.tsx
+++ src/components/Carrinho/ProdutosCarrinho.tsx
@@ -1,27 +1,97 @@
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
+import { FaTrash } from "react-icons/fa";
 
 interface Produto {
     nome: string;
     preco: number;
     imagem: { src: string; alt: string };
+    qtd: number;
 }
 
-export default function ProdutosCarrinho({ imagem, nome, preco }: Produto) {
+export default function ProdutosCarrinho({
+    imagem,
+    nome,
+    preco,
+    qtd,
+}: Produto) {
+    const [quantidade, setQuantidade] = useState(qtd);
     return (
-        <div className="flex h-fit w-4/6 flex-row bg-slate-200 p-4 max-lg:w-full dark:bg-zinc-700">
-            <div className="h-fit w-fit border-2 border-slate-700 dark:border-zinc-400">
-                <Image
-                    width={100}
-                    height={100}
-                    src={imagem.src}
-                    alt={imagem.alt}
-                />
+        <>
+            <div className="flex h-fit flex-col gap-4 bg-slate-200 p-4 dark:bg-zinc-700">
+                <div className="flex h-fit w-full flex-row">
+                    <div className="max-w-1/2 h-fit">
+                        <Image
+                            className="border-2 border-slate-700 dark:border-zinc-400"
+                            width={150}
+                            height={150}
+                            src={imagem.src}
+                            alt={imagem.alt}
+                        />
+                    </div>
+                    <div className="flex w-1/2 flex-col justify-center pl-4">
+                        <p className="text-limited-1 font-semibold">{nome}</p>
+                        <p className="text-limited-1 font-bold">{preco}</p>
+                    </div>
+                </div>
+                <div className="flex flex-row items-center justify-between">
+                    <div>
+                        <button
+                            disabled={quantidade === 1}
+                            className="rounded-full bg-white p-1 dark:bg-zinc-800"
+                            onClick={() => setQuantidade(quantidade - 1)}
+                        >
+                            <svg
+                                viewBox="0 0 1024 1024"
+                                width="1em"
+                                height="1em"
+                                fill="rgb(var(--foreground-rgb))"
+                                aria-hidden="false"
+                                focusable="false"
+                            >
+                                <path d="M170.666667 506.666667c0-20.608 16.725333-37.333333 37.333333-37.333334h608a37.333333 37.333333 0 1 1 0 74.666667H208A37.333333 37.333333 0 0 1 170.666667 506.666667z"></path>
+                            </svg>
+                        </button>
+                        <input
+                            type="text"
+                            value={quantidade}
+                            onKeyPress={(event) => {
+                                if (isNaN(Number(event.key))) {
+                                    event.preventDefault();
+                                }
+                            }}
+                            autoComplete="off"
+                            inputMode="numeric"
+                            className="bg-transparent text-center text-[rgb(var(--foreground-rgb))] outline-none"
+                            size={quantidade.toString().length}
+                            onChange={(e) =>
+                                setQuantidade(Number(e.target.value))
+                            }
+                        />
+                        <button
+                            disabled={quantidade === 1}
+                            className="rounded-full bg-white p-1 dark:bg-zinc-800"
+                            onClick={() => setQuantidade(quantidade + 1)}
+                        >
+                            <svg
+                                viewBox="0 0 1024 1024"
+                                width="1em"
+                                height="1em"
+                                fill="rgb(var(--foreground-rgb))"
+                                aria-hidden="false"
+                                focusable="false"
+                            >
+                                <path d="M522.666667 844.8c-21.333333 0-38.4-17.066667-38.4-38.4l2.133333-270.933333-270.933333 2.133333c-21.333333 0-36.266667-17.066667-38.4-36.266667 0-21.333333 17.066667-38.4 36.266666-38.4l270.933334-2.133333 2.133333-270.933333c0-21.333333 17.066667-36.266667 38.4-36.266667s36.266667 17.066667 36.266667 38.4l-2.133334 270.933333 270.933334-2.133333c21.333333 0 36.266667 17.066667 38.4 36.266667 0 21.333333-17.066667 38.4-36.266667 38.4l-270.933333 2.133333-2.133334 270.933333c2.133333 19.2-14.933333 36.266667-36.266666 36.266667z"></path>
+                            </svg>
+                        </button>
+                    </div>
+                    <div>
+                        <button className=" transition-all hover:scale-110 hover:text-red-700 active:scale-95">
+                            <FaTrash />
+                        </button>
+                    </div>
+                </div>
             </div>
-            <div className="flex flex-col p-4">
-                <p className="text-limited-1">{nome}</p>
-                <p className="text-limited-1">{preco}</p>
-            </div>
-        </div>
+        </>
     );
 }
