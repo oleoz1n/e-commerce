a75752e8ee644ce0a8d6dc8e03851ef185d1eda3
oleoz1n <leopity0@gmail.com>
2024-03-31 11:12:20 -0300

    Refactor checkProduto function and add PUT route for updating a product

--- src/app/api/produtos/[id]/route.tsx
+++ src/app/api/produtos/[id]/route.tsx
@@ -3,7 +3,6 @@ import { promises as fs } from "fs";
 import { NextResponse } from "next/server";
 
 const checkProduto = async (produto: any) => {
-    console.log(produto.desc);
     if (
         produto.nome == undefined ||
         produto.desc == undefined ||
@@ -11,7 +10,8 @@ const checkProduto = async (produto: any) => {
         produto.tipo == undefined ||
         produto.marca == undefined ||
         produto.imagem.src == undefined ||
-        produto.imagem.alt == undefined
+        produto.imagem.alt == undefined ||
+        produto.id == undefined
     ) {
         return false;
     }
@@ -36,25 +36,31 @@ export async function GET(
     return NextResponse.json({ error: "Produto não encontrado" });
 }
 
-export async function POST(request: Request, response: Response) {
+export async function PUT(
+    request: Request,
+    { params }: { params: { id: string } },
+) {
     const file = await fs.readFile(
         process.cwd() + "/src/app/api/produtos/produtos.json",
-        "utf-8",
+        "utf8",
     );
-
-    const produtos = await JSON.parse(file);
+    const produtos = JSON.parse(file);
     const produtoRequest = await request.json();
 
+    produtoRequest.id = parseInt(params.id);
     if (!(await checkProduto(produtoRequest))) {
         return NextResponse.json({ error: "Produto inválido" });
     }
-
-    produtoRequest.id = produtos[produtos.length - 1].id + 1;
-    produtos.push(produtoRequest);
-    await fs.writeFile(
-        process.cwd() + "/src/app/api/produtos/produtos.json",
-        JSON.stringify(produtos),
+    const index = produtos.findIndex(
+        (produto: Produto) => produto.id === parseInt(params.id),
     );
-
-    return NextResponse.json({ body: produtoRequest, status: 201 });
+    if (produtos[index] != undefined) {
+        produtos[index] = produtoRequest;
+        await fs.writeFile(
+            process.cwd() + "/src/app/api/produtos/produtos.json",
+            JSON.stringify(produtos),
+        );
+        return NextResponse.json({ sucess: produtoRequest });
+    }
+    return NextResponse.json({ error: "Produto não encontrado" });
 }
