77f5a83757ab13ea54bade96fdf38965d494cc6d
oleoz1n <leopity0@gmail.com>
2024-03-31 10:42:34 -0300

    Add checkProduto function and POST route for creating new products

--- src/app/api/produtos/[id]/route.tsx
+++ src/app/api/produtos/[id]/route.tsx
@@ -2,6 +2,22 @@ import Produto from "@/interface/Produto";
 import { promises as fs } from "fs";
 import { NextResponse } from "next/server";
 
+const checkProduto = async (produto: any) => {
+    console.log(produto.desc);
+    if (
+        produto.nome == undefined ||
+        produto.desc == undefined ||
+        produto.preco == undefined ||
+        produto.tipo == undefined ||
+        produto.marca == undefined ||
+        produto.imagem.src == undefined ||
+        produto.imagem.alt == undefined
+    ) {
+        return false;
+    }
+    return true;
+};
+
 export async function GET(
     request: Request,
     { params }: { params: { id: string } },
@@ -18,4 +34,27 @@ export async function GET(
         return NextResponse.json({ sucess: produto });
     }
     return NextResponse.json({ error: "Produto não encontrado" });
+}
+
+export async function POST(request: Request, response: Response) {
+    const file = await fs.readFile(
+        process.cwd() + "/src/app/api/produtos/produtos.json",
+        "utf-8",
+    );
+
+    const produtos = await JSON.parse(file);
+    const produtoRequest = await request.json();
+
+    if (!(await checkProduto(produtoRequest))) {
+        return NextResponse.json({ error: "Produto inválido" });
+    }
+
+    produtoRequest.id = produtos[produtos.length - 1].id + 1;
+    produtos.push(produtoRequest);
+    await fs.writeFile(
+        process.cwd() + "/src/app/api/produtos/produtos.json",
+        JSON.stringify(produtos),
+    );
+
+    return NextResponse.json({ body: produtoRequest, status: 201 });
 }
