3c185ab6d6e976c110261337064e9a130c6580e2
oleoz1n <leopity0@gmail.com>
2024-03-31 11:16:29 -0300

    Add DELETE function to delete a product

--- src/app/api/produtos/[id]/route.tsx
+++ src/app/api/produtos/[id]/route.tsx
@@ -64,3 +64,27 @@ export async function PUT(
     }
     return NextResponse.json({ error: "Produto não encontrado" });
 }
+
+export async function DELETE(
+    request: Request,
+    { params }: { params: { id: string } },
+) {
+    const file = await fs.readFile(
+        process.cwd() + "/src/app/api/produtos/produtos.json",
+        "utf8",
+    );
+    const produtos = JSON.parse(file);
+    const index = produtos.findIndex(
+        (produto: Produto) => produto.id === parseInt(params.id),
+    );
+    const produto = produtos[index];
+    if (produtos[index] != undefined) {
+        produtos.splice(index, 1);
+        await fs.writeFile(
+            process.cwd() + "/src/app/api/produtos/produtos.json",
+            JSON.stringify(produtos),
+        );
+        return NextResponse.json({ sucess: "Produto deletado", body: produto });
+    }
+    return NextResponse.json({ error: "Produto não encontrado" });
+}
