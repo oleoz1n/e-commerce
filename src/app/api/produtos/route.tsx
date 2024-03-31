7694656b1c46a27635dd2dca43dfd71e9955e1c1
oleoz1n <leopity0@gmail.com>
2024-03-31 11:12:16 -0300

    Add checkProduto function and POST route for creating new products

--- src/app/api/produtos/route.tsx
+++ src/app/api/produtos/route.tsx
@@ -1,6 +1,21 @@
 import { promises as fs } from "fs";
 import { NextResponse } from "next/server";
 
+const checkProduto = async (produto: any) => {
+    if (
+        produto.nome == undefined ||
+        produto.desc == undefined ||
+        produto.preco == undefined ||
+        produto.tipo == undefined ||
+        produto.marca == undefined ||
+        produto.imagem.src == undefined ||
+        produto.imagem.alt == undefined
+    ) {
+        return false;
+    }
+    return true;
+};
+
 export async function GET() {
     const file = await fs.readFile(
         process.cwd() + "/src/app/api/produtos/produtos.json",
@@ -8,4 +23,27 @@ export async function GET() {
     );
     const data = JSON.parse(file);
     return NextResponse.json(data);
+}
+
+export async function POST(request: Request, response: Response) {
+    const file = await fs.readFile(
+        process.cwd() + "/src/app/api/produtos/produtos.json",
+        "utf-8",
+    );
+
+    const produtos = await JSON.parse(file);
+    const produtoRequest = await request.json();
+
+    if (!(await checkProduto(produtoRequest))) {
+        return NextResponse.json({ error: "Produto inválido" });
+    }
+
+    produtoRequest.id = produtos[produtos.length - 1].id + 1;
+    produtos.push(produtoRequest);
+    await fs.writeFile(
+        process.cwd() + "/src/app/api/produtos/produtos.json",
+        JSON.stringify(produtos),
+    );
+
+    return NextResponse.json({ body: produtoRequest, status: 201 });
 }
